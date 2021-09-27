5d3c8792d07bfaef33246661cae85eed159f3c3c
edwinmursia <edwin.mursia@edu.turkuamk.fi>
2021-09-27 23:10:09 +0300

    Made the hamburger icon functional and created the home and hero sections.

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElements'
 import { FaBars } from 'react-icons/fa'
 
-const Navbar = () => {
+const Navbar = ({toggle}) => {
     return (
         <>
             <Nav>
                 <NavbarContainer>
                     <NavLogo to='/'>Edu</NavLogo>
-                    <MobileIcon>
+                    <MobileIcon onClick={toggle} >
                         <FaBars />
                     </MobileIcon>
                     <NavMenu>
