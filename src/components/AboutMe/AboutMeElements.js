a863c02a1e4d7cbabfeeec93d3b7f7fecb62b81b
edwinmursia <edwin.mursia@edu.turkuamk.fi>
2021-10-03 16:54:37 +0300

    Styled the AboutMe component.

--- src/components/AboutMe/AboutMeElements.js
+++ src/components/AboutMe/AboutMeElements.js
@@ -2,8 +2,9 @@ import styled from "styled-components";
 
 export const AboutMeContainer = styled.div`
     display: flex;
+    flex-direction: column;
     height: 800px;
-    background: #fff;
+    background: #cfe2f3;
     justify-content: center;
     align-items: center;
 `
@@ -14,11 +15,12 @@ export const AboutMeWrapper = styled.div`
     width: 1200px;
     display: grid;
     grid-template-columns: 1fr 1fr;
+    box-shadow: 5px 5px;
 `
 
 export const TextContainer = styled.div`
     display: flex;
-    background: #d8dbd7;
+    background: #01bf71;
     max-height: 100%;
     max-width: 100%;
 `
@@ -37,12 +39,22 @@ export const Text = styled.p`
     font-size: 18px;
     margin-top: auto;
     margin-bottom: auto;
-    margin-left: 15px;
-    margin-right: 15px;
+    margin-left: 30px;
+    margin-right: 30px;
     position: relative;
+    font-weight: bold;
+    color: #fff;
+    text-shadow: 1px 1px #000;
 `
 
 export const Image = styled.img`
     width: 296px;
     height: 363px;
+    box-shadow: 4px 4px;
+`
+
+export const HeaderText = styled.h1`
+    font-size: 40px;
+    margin-bottom: 70px;
+    text-shadow: 3px 3px #fff;
 `
--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,36 +1,38 @@
 import React from "react";
 import {
-    AboutMeContainer,
-    AboutMeWrapper,
-    ImageContainer,
-    TextContainer,
-    Text,
-    Image
+  AboutMeContainer,
+  AboutMeWrapper,
+  ImageContainer,
+  TextContainer,
+  Text,
+  Image,
+  HeaderText,
 } from "./AboutMeElements";
 
 const AboutMe = () => {
-    return (
-        <>
-            <AboutMeContainer>
-                <AboutMeWrapper>
-                    <TextContainer>
-                        <Text>
-                            My name is Edwin Mursia. I am 23 years old and I am a third year
-                            information technology student. I am originally from Helsinki but
-                            I moved to Turku because of my studies. I started to learn coding
-                            a little bit before starting my studies. I am always willing to
-                            learn more about coding and I enjoy it a lot. In my free time I
-                            like to go to the gym, spend time with people close to me, play
-                            videogames and watch sports.
-                        </Text>
-                    </TextContainer>
-                    <ImageContainer>
-                        <Image src={require('../../images/edwin.PNG').default} />
-                    </ImageContainer>
-                </AboutMeWrapper>
-            </AboutMeContainer>
-        </>
-    );
+  return (
+    <>
+      <AboutMeContainer>
+        <HeaderText>Hello! My name is</HeaderText>
+        <AboutMeWrapper>
+          <TextContainer>
+            <Text>
+              Edwin Mursia. I am 23 years old and I am a third year
+              information technology student. I am originally from Helsinki but
+              I moved to Turku because of my studies. I started to learn coding
+              a little bit before starting my studies. I am always willing to
+              learn more about coding and I enjoy it a lot. In my free time I
+              like to go to the gym, spend time with people close to me, play
+              videogames and watch sports.
+            </Text>
+          </TextContainer>
+          <ImageContainer>
+            <Image src={require("../../images/edwin.PNG").default} />
+          </ImageContainer>
+        </AboutMeWrapper>
+      </AboutMeContainer>
+    </>
+  );
 };
 
 export default AboutMe;
