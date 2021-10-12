b9318c7722199e3498b1dd7363b7901e852471eb
edwinmursia <edwin.mursia@edu.turkuamk.fi>
2021-10-12 22:52:15 +0300

    Made a function which changes the arrow when clicked.

--- src/components/Collapsible/CollapsibleComponent.js
+++ src/components/Collapsible/CollapsibleComponent.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { FaArrowDown } from "react-icons/fa";
+import { FaArrowDown, FaArrowUp } from "react-icons/fa";
 import './Collapsible.css'
 
 export const ButtonWrapper = styled.div`
@@ -15,15 +15,32 @@ export const ButtonWrapper = styled.div`
     font-size: 30px;
   }
 `
+export const TheButton = styled.div``
 
 const Collapsible = (props) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isRotated, setIsRotated] = useState(false)
+
+  const onRotated = () => {
+    setIsRotated(!isRotated)
+  }
+
+  const onOpen = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const allFunctions = () => {
+    onRotated();
+    onOpen();
+  }
 
   return (
     <div className="collapsible">
       <ButtonWrapper className="toggle" >
         <h3 className="my_header" style={{paddingBottom: 25, fontSize: '20px', cursor: "pointer"}} >My programming experience</h3>
-        <FaArrowDown onClick={() => setIsOpen(!isOpen)} style={{cursor: "pointer"}} />
+        <TheButton onClick={() => allFunctions()} style={{cursor: "pointer", transition: "all 0.2s linear"}} spy={true} exact='true' >
+          {isRotated ? <FaArrowUp/> : <FaArrowDown/>}
+        </TheButton>
       </ButtonWrapper>
       <div className={isOpen ? "content show" : "content"}>{props.children}</div>
     </div>
