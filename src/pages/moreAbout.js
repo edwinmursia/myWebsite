7cbf3187931900b059e2147fc0319d723d17be14
edwinmursia <edwin.mursia@edu.turkuamk.fi>
2021-10-13 13:06:10 +0300

    Took out the Sidebar from moreAbout page.

--- src/pages/moreAbout.js
+++ src/pages/moreAbout.js
@@ -1,5 +1,4 @@
-import React, {useState} from 'react'
-import Sidebar from '../components/Sidebar'
+import React from 'react'
 import AboutMe from '../components/AboutMe'
 import Footer from '../components/Footer'
 import Collapsible from '../components/Collapsible'
@@ -7,15 +6,8 @@ import GoBack from '../components/GoBack'
 
 const MoreAboutPage = () => {
 
-    const [isOpen, setIsOpen] = useState(false)
-
-    const toggle = () => {
-        setIsOpen(!isOpen)
-    }
-
     return (
         <>
-            <Sidebar isOpen={isOpen} toggle={toggle} />
             <GoBack />
             <AboutMe />
             <Collapsible />
